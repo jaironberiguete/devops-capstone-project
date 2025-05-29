b891d07a66dc8809e866e5d9455f9a33777457ff
jaironberiguete <jaironberiguete@gmail.com>
2025-05-28 23:46:45 -0400

    fixed indenttion error

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -177,7 +177,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-        
+
     ######################################################################
     #  C O R D  T E S T   C A S E S
     ######################################################################
